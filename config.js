4522c94c26679d54ea0d834b68ecb34dd7329b2a
JRoluosangEditz <159141071+JRoluosangEditz@users.noreply.github.com>
2024-12-19 12:52:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,25 +8,25 @@ module.exports = {
     /**
      * bot details and parameters
      */
-    botname: process.env.BotName || "Qᴜᴇᴇɴ ᴀɴʏᴀ ᴠ2",
+    botname: process.env.BotName || "۝vAiPA۝",
     footer: process.env.Footer || "© 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐀𝐧𝐲𝐚 𝐁𝐨𝐭",
-    prefa: process.env.PREFIX || process.env.Prefix || "-",
+    prefa: process.env.PREFIX || process.env.Prefix || "✓",
     themeemoji: process.env.ThemeEmoji || "🎐",
 
     /**
-     * owner details and parameters
+     * This bot is made by J Rohluosang Editz 
      */
-    ownername: process.env.OWNER_NUMBER || process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.OWNER_NUMBER || process.env.Owner_Name || "۝vAiPA۝",
+    ownernumber: process.env.Owner_Number || "917005658594",
+    instagramId: process.env.Insta || "ka_ning_ka_kal_dawn",
 
     /**
      * other details and parameters
      */
     author: process.env.Author || "𝐏𝐢𝐤𝐚𝐁𝐨𝐭𝐳 🌚🌙",
-    packname: process.env.PackName || "𝕼𝖚𝖊𝖊𝖓 𝕬𝖓𝖞𝖆 𝖁2 𝕾𝖙𝖎𝖈𝖐𝖊𝖗 𝕻𝖆𝖈𝖐 🪀",
-    socialLink: process.env.Web || "https://github.com/PikaBotz",
-    groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
+    packname: process.env.PackName || "۝vAiPA۝",
+    socialLink: process.env.Web || "https://github.com/JRohluosangEditz",
+    groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGt",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
